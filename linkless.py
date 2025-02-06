650cc7d43145819ed99204077e7edfef83ac6ae2
Saenaru <Nyanja230@gmail.com>
2025-02-07 02:22:19 +0300

    Update linkless.py

--- linkless.py
+++ linkless.py
@@ -1,23 +1,24 @@
 import os
 import requests
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 
 def shorten_link(token, url):
-    url_link = "https://api.vk.com/method/utils.getShortLink"
+    link_url = "https://api.vk.com/method/utils.getShortLink"
     params = {
         "access_token": token,
         "v": "5.199",
         "url": url,
     }
-    response = requests.get(url_link, params=params)
+    response = requests.get(link_url, params=params)
     response.raise_for_status()
     data = response.json()
     return data["response"]["short_url"]
 
 
 def count_clicks(token, short_url):
-    url_stats = "https://api.vk.com/method/utils.getLinkStats"
+    stats_url = "https://api.vk.com/method/utils.getLinkStats"
     key = short_url.split('/')[-1]
     params = {
         "access_token": token,
@@ -25,7 +26,7 @@ def count_clicks(token, short_url):
         "interval": "forever",
         "v": "5.199",
     }
-    response = requests.get(url_stats, params=params)
+    response = requests.get(stats_url, params=params)
     response.raise_for_status()
     data = response.json()
     if "response" in data and "stats" in data["response"] and data["response"]["stats"]:
@@ -34,25 +35,23 @@ def count_clicks(token, short_url):
         return 0
 
 
-def is_short_link(url):
-    short_prefix = "https://vk.cc/"
-    return url.startswith(short_prefix)
+def is_short_link(token, url):
+    return urlparse(url).netloc == "vk.cc"
 
 
 def main():
     load_dotenv("token.env")
-    token = os.getenv("TOKEN")
-    url_user = input("Введите ссылку: ")
+    token = os.environ["API_VK_TOKEN"]
+    user_url = input("Введите ссылку: ")
     try:
-        if is_short_link(url_user):
-            clicks = count_clicks(token, url_user)
+        if is_short_link(token, user_url):
+            clicks = count_clicks(token, user_url)
             print('Количество кликов по ссылке: ', clicks)
         else:
-            response = requests.get(url_user)
+            response = requests.get(user_url)
             response.raise_for_status()
-            short_link = shorten_link(token, url_user)
+            short_link = shorten_link(token, user_url)
             print('Сокращённая ссылка: ', short_link)
-            short_link = shorten_link(token, url_user)
     except requests.exceptions.HTTPError as error:
         print(f"Данные с сервера недоступны:\n{error}")
 
